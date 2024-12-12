44fce702b63ebcb2f380ae713e053fcc49f1aba2
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2024-12-12 05:17:31 +0200

    Sum Each Col In Matrix In Another Array

---
+++ Problem5/Problem5.cs
@@ -0,0 +1,113 @@
+﻿namespace Problem5
+{
+    internal class Problem5
+    {
+
+        // Sum Each Col In Matrix In Another Array
+
+        static Random random = new Random();
+
+        static int RandomNumbers(int from, int to)
+        {
+
+            return random.Next(from, to);
+
+        }
+
+        static void FillMatrixWithRandomNumbers(int[,] matrix)
+        {
+
+            for (short i = 0; i < matrix.GetLength(0); i++)
+            {
+
+                for (short j = 0; j < matrix.GetLength(1); j++)
+                {
+
+                    matrix[i, j] = RandomNumbers(1, 100);
+
+                }
+
+            }
+
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+
+            Console.WriteLine("\nThe following is a 3x3 random matrix:");
+
+            for (short i = 0; i < matrix.GetLength(0); i++)
+            {
+
+                for (short j = 0; j < matrix.GetLength(1); j++)
+                {
+
+                    Console.Write($"{matrix[i, j],3}   ");
+
+                }
+
+                Console.WriteLine();
+
+            }
+
+        }
+
+        static int ColSum(int[,] matrix, short colNumber)
+        {
+
+            int sum = 0;
+
+            for (short i = 0; i < matrix.GetLength(0); i++)
+            {
+
+                sum += matrix[i, colNumber];
+
+            }
+
+            return sum;
+
+        }
+
+        static void SumMatrixColsInArray(int[,] matrix, int[] arrSum)
+        {
+
+            for (short j = 0; j < matrix.GetLength(1); j++)
+            {
+
+                arrSum[j] = ColSum(matrix, j);
+
+            }
+
+        }
+
+        static void PrintColsSumArray(int[] arrSum)
+        {
+
+            Console.WriteLine("\nThe following is a Sum Each Row In Matrix:");
+
+            for(short i=0; i < arrSum.Length; i++)
+            {
+
+                Console.WriteLine($" Col {i+1} Sum = " + arrSum[i]);
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            int[,] matrix = new int[3, 3];
+            int[] arrSum = new int[3];
+
+            FillMatrixWithRandomNumbers(matrix);
+
+            PrintMatrix(matrix);
+
+            SumMatrixColsInArray(matrix, arrSum);
+
+            PrintColsSumArray(arrSum);         
+
+        }
+    }
+}
