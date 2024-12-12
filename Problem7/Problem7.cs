f25f27e9a7609e270f14f4c9a5106553df998017
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2024-12-12 09:41:36 +0200

    Transpose Matrix

---
+++ Problem7/Problem7.cs
@@ -0,0 +1,85 @@
+﻿namespace Problem7
+{
+    internal class Problem7
+    {
+
+        // Transpose Matrix
+
+        static void FillMatrixWithRandomNumbers(int[,] matrix)
+        {
+
+            short counter = 0;
+
+            for (short i = 0; i < matrix.GetLength(0); i++)
+            {
+
+                for (short j = 0; j < matrix.GetLength(1); j++)
+                {
+
+                    counter++;
+                    matrix[i, j] = counter;
+
+                }
+
+            }
+
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+
+            for (short i = 0; i < matrix.GetLength(0); i++)
+            {
+
+                for (short j = 0; j < matrix.GetLength(1); j++)
+                {
+
+                    Console.Write($"{matrix[i, j],2}   ");
+
+                }
+
+                Console.WriteLine();
+
+            }
+
+
+        }
+
+        static void TransposeMatrix(int[,] matrix, int[,] matrixTransposed)
+        {
+
+            for (short i = 0; i < matrix.GetLength(0); i++)
+            {
+
+                for (short j = 0; j < matrix.GetLength(1); j++)
+                {
+
+                    matrixTransposed[i, j] = matrix[j, i];
+
+                }
+
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            int[,] matrix = new int[3, 3];
+            int[,] matrixTransposed = new int[3, 3];
+
+
+            FillMatrixWithRandomNumbers(matrix);
+
+            Console.WriteLine("\nThe following is a 3x3 matrix:");
+            PrintMatrix(matrix);
+
+            TransposeMatrix(matrix, matrixTransposed);
+
+            Console.WriteLine("\nThe following is the transposed matrix:");
+            PrintMatrix(matrixTransposed);
+
+        }
+    }
+}
