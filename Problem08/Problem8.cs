982472cf5231c076653dd1e644e1a7bb3ba23b75
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2024-12-14 10:58:19 +0200

    Multiply Two Matrices

--- Problem08/Problem8.cs
+++ Problem08/Problem8.cs
@@ -1,27 +1,29 @@
-﻿namespace Problem8
+namespace Problem8
 {
     internal class Problem8
     {
+
+        // Multiply Two Matrices
 
         static Random random = new Random();
 
         static int RandomNumber(int from, int to)
         {
 
-            return random.Next(from, to);
+            return random.Next(from, to + 1);
 
         }
 
         static void FillMatrixWithRandomNumbers(int[,] matrix)
         {
 
-            for(short i =0; i < matrix.GetLength(0); i++)
+            for (short i = 0; i < matrix.GetLength(0); i++)
             {
 
-                for(short j = 0; j < matrix.GetLength(1); j++)
+                for (short j = 0; j < matrix.GetLength(1); j++)
                 {
 
-                    matrix[i, j] = RandomNumber(1, 11);
+                    matrix[i, j] = RandomNumber(1, 10);
 
                 }
 
@@ -38,7 +40,7 @@
                 for (short j = 0; j < matrix.GetLength(1); j++)
                 {
 
-                    Console.Write($" {matrix[i,j]:D2}   ");
+                    Console.Write($" {matrix[i, j]:D2}   ");
 
                 }
 
@@ -57,7 +59,7 @@
                 for (short j = 0; j < matrix1.GetLength(1); j++)
                 {
 
-                    matrixResults[i, j] = matrix1[i, j] * matrix2[i,j];
+                    matrixResults[i, j] = matrix1[i, j] * matrix2[i, j];
 
                 }
 
