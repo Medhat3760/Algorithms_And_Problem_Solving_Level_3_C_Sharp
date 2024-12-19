61715bce6aa53e44dad7df6bd2a8b0cc2a9199ad
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2024-12-19 14:20:58 +0200

    Check Scalar Matrix

---
+++ Problem14/Problem14.cs
@@ -0,0 +1,86 @@
+﻿namespace Problem14
+{
+    // Check Scalar Matrix
+    internal class Problem14
+    {
+
+        static void PrintMatrix(int[,] matrix)
+        {
+
+            for (short i = 0; i < matrix.GetLength(0); i++)
+            {
+
+                for (short j = 0; j < matrix.GetLength(1); j++)
+                {
+
+                    Console.Write($" {matrix[i, j],2}   ");
+
+                }
+
+                Console.WriteLine();
+
+            }
+
+        }
+
+        static bool IsScalarMatrix(int[,] matrix)
+        {
+
+            int firstDiagElement = matrix[0, 0];
+
+            for (short i = 0; i < matrix.GetLength(0); i++)
+            {
+
+                for (short j = 0; j < matrix.GetLength(1); j++)
+                {
+
+                    if (i == j && matrix[i, j] != firstDiagElement)
+                    {
+
+                        return false;
+
+                    }
+                    else if (i != j && matrix[i, j] != 0)
+                    {
+
+                        return false;
+
+                    }
+
+                }
+
+            }
+
+            return true;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            int[,] matrix1 =
+            {
+                {9, 0, 0 },
+                {0, 9, 0 },
+                {2, 0, 9 }
+            };
+
+            Console.WriteLine("\nMatrix1:");
+            PrintMatrix(matrix1);
+
+            if (IsScalarMatrix(matrix1))
+            {
+
+                Console.WriteLine("\nYes: matrix is scalar.");
+
+            }
+            else
+            {
+
+                Console.WriteLine("\nNo: matrix is NOT scalar");
+
+            }
+
+        }
+    }
+}
