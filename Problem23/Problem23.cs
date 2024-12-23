8e57594e18e4a6201cd591e8d4268ce830846cd2
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2024-12-23 22:39:47 +0200

    Print First Letter of Each Word

---
+++ Problem23/Problem23.cs
@@ -0,0 +1,45 @@
+﻿namespace Problem23
+{
+    // Print First Letter of Each Word
+    internal class Problem23
+    {
+
+        static string ReadString(string message)
+        {
+
+            string text = "";
+
+            Console.WriteLine(message);
+            text = Console.ReadLine();
+
+            return text;
+
+        }
+
+        static void PrintFirstLetterOfEachWord(string text)
+        {
+
+            Console.WriteLine("First Letter Of Each Word:");
+
+            bool isFirstLetter = true;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+
+                if (text[i] != ' ' && isFirstLetter)
+                {
+                    Console.WriteLine(text[i]);
+                }
+
+                isFirstLetter = (text[i] == ' ') ? true : false;
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            PrintFirstLetterOfEachWord(ReadString("Please Enter Your String?"));
+        }
+    }
+}
