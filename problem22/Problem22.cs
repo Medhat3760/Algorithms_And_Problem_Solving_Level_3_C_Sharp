88e77e11728bc08443f3f048e826e2e06a2f5df7
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2024-12-23 19:33:05 +0200

    Fibonacci Series With Recursion

---
+++ problem22/Problem22.cs
@@ -0,0 +1,33 @@
+﻿namespace Problem22
+{
+    // Fibonacci Series With Recursion
+    internal class Problem22
+    {
+
+        static void PrintFibonacciUsinRecursion(int number, int prev1, int prev2)
+        {
+
+            int fibNumber = 0;
+
+            if (number > 0)
+            {
+
+                fibNumber = prev1 + prev2;
+                Console.Write($" {fibNumber} ");
+                prev2 = prev1;
+                prev1 = fibNumber;
+
+                PrintFibonacciUsinRecursion(number - 1, prev1, prev2);
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            PrintFibonacciUsinRecursion(10, 0, 1);
+
+        }
+    }
+}
