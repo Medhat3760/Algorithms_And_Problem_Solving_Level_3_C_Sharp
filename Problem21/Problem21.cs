022e8dfe5d9ad458bdd4889186aad2ab79c4e20e
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2024-12-23 19:02:06 +0200

    Fibonacci Series

---
+++ Problem21/Problem21.cs
@@ -0,0 +1,33 @@
+﻿namespace Problem21
+{
+    // Fibonacci Series
+    internal class Problem21
+    {
+
+        static void PrintFibonacciUsingLoop(int number)
+        {
+
+            int febNumber = 0;
+            int prev2 = 0, prev1 = 1;
+
+            Console.Write(" 1 ");
+            for (int i = 2; i <= number; i++)
+            {
+
+                febNumber = prev1 + prev2;
+                Console.Write($" {febNumber} ");
+                prev2 = prev1;
+                prev1 = febNumber;
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            PrintFibonacciUsingLoop(10);
+
+        }
+    }
+}
