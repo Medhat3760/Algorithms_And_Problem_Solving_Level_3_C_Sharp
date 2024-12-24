d860f20441739e33805e1c1c18f5c46f38a581f6
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2024-12-24 20:56:55 +0200

    Invert Character Case

---
+++ Problem27/Problem27.cs
@@ -0,0 +1,37 @@
+﻿namespace Problem27
+{
+    // Invert Character Case
+    internal class Problem27
+    {
+
+        static char ReadCharacter(string message)
+        {
+
+            char c;
+
+            Console.WriteLine(message);
+            c = char.Parse(Console.ReadLine());
+
+            return c;
+
+        }
+
+        static char InvertCharacterCase(char c)
+        {
+
+            return char.IsUpper(c) ? char.ToLower(c) : char.ToUpper(c);
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            char ch1 = ReadCharacter("Please Enter a Character?");
+
+            Console.WriteLine("\nCharacter After Inverting Case:");
+            ch1 = InvertCharacterCase(ch1);
+            Console.WriteLine(ch1);
+
+        }
+    }
+}
