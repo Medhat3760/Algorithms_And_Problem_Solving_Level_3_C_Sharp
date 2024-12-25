2c3657cadc7efba71d5c2d569aa6cafbd8aa1c24
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2024-12-25 21:45:00 +0200

    Count Letters ( Match Case )

---
+++ Problem31/Problem31.cs
@@ -0,0 +1,79 @@
+﻿namespace Problem31
+{
+    // Count Letters ( Match Case )
+    internal class Problem31
+    {
+
+        static string ReadString(string message)
+        {
+
+            string text = "";
+
+            Console.WriteLine(message);
+            text = Console.ReadLine();
+            return text;
+
+        }
+
+        static char ReadChar(string message)
+        {
+
+            char c;
+
+            Console.WriteLine(message);
+            c = char.Parse(Console.ReadLine());
+
+            return c;
+
+        }
+
+        static char InvertLetterCase(char c)
+        {
+
+            return char.IsUpper(c) ? char.ToLower(c) : char.ToUpper(c);
+
+        }
+
+        static short CountSpecificLetter(string text, char letter, bool matchCase = true)
+        {
+
+            short counter = 0;
+
+            for (short i = 0; i < text.Length; i++)
+            {
+                if (matchCase)
+                {
+                    if (text[i] == letter)
+                    {
+                        counter++;
+                    }
+                }
+                else
+                {
+
+                    if (char.ToLower(text[i]) == char.ToLower(letter))
+                    {
+                        counter++;
+                    }
+
+                }
+            }
+
+            return counter;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            string s1 = ReadString("Please Enter Your String?");
+
+            char c1 = ReadChar("\nPlease Enter Your Character?");
+
+            Console.WriteLine("\nLetter '" + c1 + "' Count = " + CountSpecificLetter(s1, c1));
+
+            Console.WriteLine($"\nLetter '{c1}' and '{InvertLetterCase(c1)}' Count = " + CountSpecificLetter(s1, c1, false));
+
+        }
+    }
+}
