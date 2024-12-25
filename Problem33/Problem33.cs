74b720203c9cd1071aaec538e18885dee832e73f
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2024-12-25 23:28:38 +0200

    Count Vowels

---
+++ Problem33/Problem33.cs
@@ -0,0 +1,55 @@
+﻿namespace Problem33
+{
+    // Count Vowels
+    internal class Problem33
+    {
+
+        static string ReadString(string message)
+        {
+
+            string text = "";
+
+            Console.WriteLine(message);
+            text = Console.ReadLine();
+            return text;
+
+        }
+
+        static bool IsVowel(char c)
+        {
+
+            c = char.ToLower(c);
+
+            return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+
+        }
+
+        static short CountVowels(string text)
+        {
+
+            short counter = 0;
+
+            for (short i = 0; i < text.Length; i++)
+            {
+
+                if (IsVowel(text[i]))
+                {
+                    counter++;
+                }
+
+            }
+
+            return counter;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            string s1 = ReadString("Please Enter Your String?");
+
+            Console.WriteLine("\nNumber Of Vowels is: " + CountVowels(s1));
+
+        }
+    }
+}
