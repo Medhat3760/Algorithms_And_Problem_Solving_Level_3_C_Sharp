6e70b399ede33149b5d83ce3b543adc5bed7f139
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2024-12-25 20:29:59 +0200

    Count Letter

---
+++ Problem30/Problem30.cs
@@ -0,0 +1,58 @@
+﻿namespace Problem30
+{
+    // Count Letter
+    internal class Problem30
+    {
+
+        static string ReadString(string message)
+        {
+
+            string text = "";
+
+            Console.WriteLine(message);
+            text = Console.ReadLine();
+            return text;
+
+        }
+
+        static char ReadChar(string message)
+        {
+
+            char c;
+
+            Console.WriteLine(message);
+            c = char.Parse(Console.ReadLine());
+
+            return c;
+
+        }
+
+        static short CountSpecificLetter(string text, char letter)
+        {
+            short counter = 0;
+            for (short i = 0; i < text.Length; i++)
+            {
+
+                if (text[i] == letter)
+                {
+                    counter++;
+                }
+
+            }
+
+            return counter;
+
+        }
+
+        static void Main()
+        {
+
+            string s1 = ReadString("Please Enter Your String?");
+
+            char c1 = ReadChar("\nPlease Enter Your Character?");
+
+            Console.WriteLine("\nLetter '" + c1 + "' Count = " + CountSpecificLetter(s1, c1));
+
+        }
+    }
+}
