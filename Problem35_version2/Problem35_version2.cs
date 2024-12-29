869abbbbf8240f0d18c0f2fcb444dd540ae4dbce
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2024-12-29 23:42:39 +0200

    Print Each Word In String

---
+++ Problem35_version2/Problem35_version2.cs
@@ -0,0 +1,47 @@
+﻿namespace Problem35_version2
+{
+    // Print Each Word In String
+    internal class Problem35_version2
+    {
+
+        static string ReadString(string message)
+        {
+
+            string text;
+
+            Console.WriteLine(message);
+            text = Console.ReadLine();
+
+            return text;
+
+        }
+
+        static void PrintEachWordInString(string text)
+        {
+
+            Console.WriteLine("\nYour string words are: ");
+
+            for (short i = 0; i < text.Length; i++)
+            {
+
+                if (text[i] == ' ')
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write(text[i]);
+                }
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            PrintEachWordInString(ReadString("Please Enter Your Text?"));
+
+        }
+    }
+}
