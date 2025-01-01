aaac300e9b20b8de2d19c6c303e66184a2b4565a
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-01-01 23:27:53 +0200

    TrimLift, TrimRight, Trim

---
+++ Problem38/Problem38.cs
@@ -0,0 +1,73 @@
+﻿namespace Problem38
+{
+    // TrimLift, TrimRight, Trim
+    internal class Problem38
+    {
+
+        static string ReadString(string message)
+        {
+
+            string text;
+
+            Console.WriteLine(message);
+            text = Console.ReadLine();
+
+            return text;
+
+        }
+
+        static string TrimLift(string text)
+        {
+
+            for (short i = 0; i < text.Length; i++)
+            {
+
+                if (text[i] != ' ')
+                {
+                    return text.Substring(i, text.Length - i);
+                }
+
+            }
+
+            return "";
+
+        }
+
+        static string TrimRight(string text)
+        {
+
+            for (short i = (short)(text.Length - 1); i > 0; i--)
+            {
+
+                if (text[i] != ' ')
+                {
+                    return text = text.Substring(0, i + 1);
+                }
+
+            }
+
+            return "";
+
+        }
+
+        static string Trim(string text)
+        {
+
+            return TrimLift(TrimRight(text));
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            string s1 = ReadString("Please Enter Your String?");
+
+            Console.WriteLine("Trim Lift = " + TrimLift(s1));
+
+            Console.WriteLine("Trim Right = " + TrimRight(s1));
+
+            Console.WriteLine("Trim = " + Trim(s1));
+
+        }
+    }
+}
