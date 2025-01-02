e410760dd23cc422c5ffa6e7e4f4a8278340c6e4
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-01-02 21:55:08 +0200

    Join String ( Overloading )

---
+++ Problem40/Problem40.cs
@@ -0,0 +1,48 @@
+﻿namespace Problem40
+{
+    // Join String ( Overloading )
+    internal class Problem40
+    {
+
+        static string JoinString(List<string> lString, string delim)
+        {
+
+            string str = "";
+
+            foreach (string s in lString)
+            {
+                str += s + delim;
+            }
+
+            return str.Substring(0, str.Length - delim.Length);
+
+        }
+
+        static string JoinString(string[] arrString, string delim)
+        {
+
+            string str = "";
+
+            for (short i = 0; i < arrString.Length; i++)
+            {
+                str += arrString[i] + delim;
+            }
+
+            return str.Substring(0, str.Length - delim.Length);
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            List<string> lString = new List<string> { "Abdelrahman", "Medhat", "Rushdy" };
+
+            string[] arrString = { "Abdelrahman", "Medhat", "Rushdy" };
+
+            Console.WriteLine("\nList after join:\n" + JoinString(lString, " "));
+
+            Console.WriteLine("\nArr after join:\n" + JoinString(arrString, " "));
+
+        }
+    }
+}
