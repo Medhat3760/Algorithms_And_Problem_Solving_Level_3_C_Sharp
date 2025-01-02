02d1ccff657db36f3c01aad7a892061aa919efb3
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-01-02 20:32:46 +0200

    Join String

---
+++ Problem39/Problem39.cs
@@ -0,0 +1,33 @@
+﻿namespace Problem39
+{
+    // Join String
+    internal class Problem39
+    {
+
+        static string JoinString(List<string> lString, string delim)
+        {
+
+            string str = "";
+
+            for (short i = 0; i < lString.Count; i++)
+            {
+                str += lString[i];
+
+                if (i != lString.Count - 1)
+                    str += delim;
+            }
+
+            return str;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            List<string> lString = new List<string> { "Abdelrahman", "Medhat", "Rushdy" };
+
+            Console.WriteLine("\nList after join: \n" + JoinString(lString, ","));
+
+        }
+    }
+}
