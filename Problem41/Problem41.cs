7ec3a9b40802fff7eb9bf46dc8bbfe9e20e6e776
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-01-03 02:00:15 +0200

    Reverse Words

---
+++ Problem41/Problem41.cs
@@ -0,0 +1,78 @@
+﻿namespace Problem41
+{
+    // Reverse Words
+    internal class Problem41
+    {
+
+        static string ReadString(string message)
+        {
+
+            string text;
+
+            Console.WriteLine(message);
+            text = Console.ReadLine();
+
+            return text;
+
+        }
+
+        static List<string> SplitString(string text, string delimiter)
+        {
+
+            List<string> lString = new List<string>();
+
+            string word = "";
+
+            short pos = 0;
+
+            while ((pos = (short)text.IndexOf(delimiter)) != -1)
+            {
+
+                word = text.Substring(0, pos);
+
+                if (word != "")
+                {
+                    lString.Add(word);
+                }
+
+                text = text.Substring(pos + delimiter.Length);
+
+            }
+
+            if (text != "")
+            {
+                lString.Add(text);
+            }
+
+            return lString;
+
+        }
+
+        static string ReverseWordsInString(string text)
+        {
+
+            string reversedWords = "";
+
+            List<string> lWords = SplitString(text, " ");
+
+            for (int i = lWords.Count - 1; i >= 0; i--)
+            {
+                reversedWords += lWords[i] + " ";
+            }
+
+            reversedWords = reversedWords.Substring(0, reversedWords.Length - 1);
+
+            return reversedWords;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            string s1 = ReadString("Please Enter Your String?");
+
+            Console.WriteLine("\nString after reversing words:\n" + ReverseWordsInString(s1));
+
+        }
+    }
+}
