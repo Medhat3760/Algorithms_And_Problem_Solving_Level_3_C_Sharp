9ea9a76b9ff5b5e4873b598fe719cf5e9f1cc8b7
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-01-07 21:54:58 +0200

    Replace Words

---
+++ Problem42_version2/Problem42_version2.cs
@@ -0,0 +1,40 @@
+﻿namespace Problem42_version2
+{
+    // Replace Words
+    internal class Problem42_version2
+    {
+
+        static string ReplaceWordInStringUsingBuiltInFunction(string text, string word, string replaceTo)
+        {
+
+            short pos = (short)text.IndexOf(word);
+
+            while (pos != -1)
+            {
+
+                text = text.Remove(pos, word.Length).Insert(pos, replaceTo);
+
+                pos = (short)text.IndexOf(word);
+
+            }
+
+            //text = text.Replace(word, replaceTo);
+
+            return text;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            string s1 = "Welcome to Egypt , Egypt is a nice country";
+
+            Console.WriteLine(s1);
+
+            Console.WriteLine("\nText After Replaced: ");
+
+            Console.WriteLine(ReplaceWordInStringUsingBuiltInFunction(s1, "Egypt", "USA"));
+
+        }
+    }
+}
