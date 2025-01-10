fb3ed2c059afc33691c50f9fdf5ac1707bc9bf7f
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-01-10 17:07:46 +0200

    Remove Punctuation

---
+++ Problem44/Problem44.cs
@@ -0,0 +1,37 @@
+﻿namespace Problem44
+{
+    // Remove Punctuation
+    internal class Problem44
+    {
+
+        static string RemovePunctuations(string text)
+        {
+
+            char[] charArr = text.ToCharArray();
+
+            for (short i = 0; i < charArr.Length; i++)
+            {
+
+                if (char.IsPunctuation(charArr[i]))
+                {
+                    charArr[i] = '\0';
+                }
+
+            }
+
+            return new string(charArr);
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            string s1 = "Welcome to Jordan, Jordan is a nice country; it's amazing.";
+
+            Console.WriteLine("\nOriginal String:\n" + s1);
+
+            Console.WriteLine("\nPunctuation Removed:\n" + RemovePunctuations(s1));
+
+        }
+    }
+}
