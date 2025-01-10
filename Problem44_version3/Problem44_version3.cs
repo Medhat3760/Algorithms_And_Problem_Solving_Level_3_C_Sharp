f7980a0ff39f10c24e802dbf23801c0148f40563
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-01-10 17:31:21 +0200

    Remove Punctuation

---
+++ Problem44_version3/Problem44_version3.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace Problem44_version3
+{
+    // Remove Punctuation
+    internal class Problem44_version3
+    {
+
+        static string RemovePunctuationsOptimized(string text)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in text)
+            {
+                if (!char.IsPunctuation(c))
+                {
+                    sb.Append(c);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+
+            string s1 = "Welcome to Jordan, Jordan is a nice country; it's amazing.";
+
+            Console.WriteLine("\nOriginal String:\n" + s1);
+
+            Console.WriteLine("\nPunctuation Removed:\n" + RemovePunctuationsOptimized(s1));
+
+        }
+    }
+}
